2c46c305bf1e8ecd3ed0057c05792e5d4411a19f
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2026-05-25 12:37:00 +0600

    job list dark mode issue fixed

--- static/script/script.js
+++ static/script/script.js
@@ -1,5 +1,8 @@
 function onChanged() {
     var toggleButton = document.getElementById('flexSwitch');
+    const job = document.getElementById('career-history');
+    const allJobs = job.querySelectorAll('*');
+
     if (toggleButton.checked) {
         // dark
         document.getElementById('mode-name').innerHTML = 'Light';
@@ -10,16 +13,20 @@ function onChanged() {
         document.getElementById('about-me').classList.remove('bg-light', 'text-dark');
         document.getElementById('about-me').classList.add('bg-dark', 'text-white-50');
 
-        document.getElementById('career-history').classList.remove('bg-light', 'text-dark');
-        document.getElementById('career-history').classList.add('bg-dark', 'text-white-50');
+
+        job.classList.remove('bg-light', 'text-dark');
+        job.classList.add('bg-dark', 'text-white-50');
+
+        allJobs.forEach(descendant => {
+            descendant.classList.remove('bg-light', 'text-dark');
+            descendant.classList.add('bg-dark', 'text-white-50');
+        });
 
         document.getElementById('my-certification').classList.remove('bg-light', 'text-dark');
         document.getElementById('my-certification').classList.add('bg-dark', 'text-white-50');
 
         document.getElementById('copyright').classList.remove('bg-secondary', 'text-light');
         document.getElementById('copyright').classList.add('bg-light', 'text-dark');
-
-
     } else {
         // light
         document.getElementById('mode-name').innerHTML = 'Dark';
@@ -30,8 +37,13 @@ function onChanged() {
         document.getElementById('about-me').classList.remove('bg-dark', 'text-white-50');
         document.getElementById('about-me').classList.add('bg-light', 'text-dark');
 
-        document.getElementById('career-history').classList.remove('bg-dark', 'text-white-50');
-        document.getElementById('career-history').classList.add('bg-light', 'text-dark');
+        job.classList.remove('bg-dark', 'text-white-50');
+        job.classList.add('bg-light', 'text-dark');
+
+        allJobs.forEach(descendant => {
+            descendant.classList.remove('bg-dark', 'text-white-50');
+            descendant.classList.add('bg-light', 'text-dark');
+        });
 
         document.getElementById('my-certification').classList.remove('bg-dark', 'text-white-50');
         document.getElementById('my-certification').classList.add('bg-light', 'text-dark');
