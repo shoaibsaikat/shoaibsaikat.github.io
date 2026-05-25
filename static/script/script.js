7b9b01b9a635169e48d2987145bffa4c5c778190
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2026-05-25 13:11:01 +0600

    applied dark light theme globally, todo: fix color changer issue on theme change

--- static/script/script.js
+++ static/script/script.js
@@ -1,54 +1,54 @@
 function onChanged() {
     var toggleButton = document.getElementById('flexSwitch');
-    const job = document.getElementById('career-history');
-    const allJobs = job.querySelectorAll('*');
+    const detail = document.getElementById('details');
+    const detailItems = detail.querySelectorAll('*');
+
+    const leftPane = document.getElementById('left-pane');
+    const leftPaneItems = leftPane.querySelectorAll('*');
 
     if (toggleButton.checked) {
         // dark
         document.getElementById('mode-name').innerHTML = 'Light';
 
-        document.body.classList.remove('bg-secondary', 'bg-opacity-25');
+        document.body.classList.remove('bg-light');
         document.body.classList.add('bg-dark');
 
-        document.getElementById('about-me').classList.remove('bg-light', 'text-dark');
-        document.getElementById('about-me').classList.add('bg-dark', 'text-white-50');
+        detail.classList.remove('bg-light', 'text-dark');
+        detail.classList.add('bg-dark', 'text-white-50');
 
-
-        job.classList.remove('bg-light', 'text-dark');
-        job.classList.add('bg-dark', 'text-white-50');
-
-        allJobs.forEach(descendant => {
+        detailItems.forEach(descendant => {
             descendant.classList.remove('bg-light', 'text-dark');
             descendant.classList.add('bg-dark', 'text-white-50');
         });
 
-        document.getElementById('my-certification').classList.remove('bg-light', 'text-dark');
-        document.getElementById('my-certification').classList.add('bg-dark', 'text-white-50');
+        leftPane.classList.remove('bg-light', 'text-dark');
+        leftPane.classList.add('bg-dark', 'text-white-50');
 
-        document.getElementById('copyright').classList.remove('bg-secondary', 'text-light');
-        document.getElementById('copyright').classList.add('bg-light', 'text-dark');
+        leftPaneItems.forEach(descendant => {
+            descendant.classList.remove('bg-light', 'text-dark');
+            descendant.classList.add('bg-dark', 'text-white-50');
+        });
     } else {
         // light
         document.getElementById('mode-name').innerHTML = 'Dark';
 
         document.body.classList.remove('bg-dark');
-        document.body.classList.add('bg-secondary', 'bg-opacity-25');
+        document.body.classList.add('bg-light');
 
-        document.getElementById('about-me').classList.remove('bg-dark', 'text-white-50');
-        document.getElementById('about-me').classList.add('bg-light', 'text-dark');
+        detail.classList.remove('bg-dark', 'text-white-50-50');
+        detail.classList.add('bg-light', 'text-dark');
 
-        job.classList.remove('bg-dark', 'text-white-50');
-        job.classList.add('bg-light', 'text-dark');
-
-        allJobs.forEach(descendant => {
+        detailItems.forEach(descendant => {
             descendant.classList.remove('bg-dark', 'text-white-50');
             descendant.classList.add('bg-light', 'text-dark');
         });
 
-        document.getElementById('my-certification').classList.remove('bg-dark', 'text-white-50');
-        document.getElementById('my-certification').classList.add('bg-light', 'text-dark');
+        leftPane.classList.remove('bg-dark', 'text-white-50');
+        leftPane.classList.add('bg-light', 'text-dark');
 
-        document.getElementById('copyright').classList.remove('bg-light', 'text-dark');
-        document.getElementById('copyright').classList.add('bg-secondary', 'text-light');
+        leftPaneItems.forEach(descendant => {
+            descendant.classList.remove('bg-dark', 'text-white-50');
+            descendant.classList.add('bg-light', 'text-dark');
+        });
     }
 }
